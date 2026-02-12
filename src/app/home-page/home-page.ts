e82c9dd920010c472c9b72cc2d61c3fb6be15ad5
NotGnarfodolfus <NotGnarfodolfus@local>
2026-02-12 22:28:07 +0100

    improve colors

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -66,10 +66,13 @@ export class HomePage {
   }
 
   getRatingColor(rating: number): string | undefined {
-    if (rating >= 4) return '#E7180B';
-    if (rating >= 2) return '#FE9A37';
+    if (rating >= 8) return '#ff0000';
+    if (rating >= 4) return '#d40e0e';
+    if (rating >= 2) return '#ab1c1c';
     if (rating >= 1) return undefined;
-    if (rating >= 0.5) return '#7CCF35';
-    else return '#05DF72';
+    if (rating >= 0.5) return '#1cab1c';
+    if (rating >= 0.25) return '#0ed40e';
+    if (rating >= 0.01) return '#00ff00';
+    else return '#77cfcf';
   }
 }
